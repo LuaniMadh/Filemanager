58f5a08a011278c4c2a9356583af0416092fcec3
LuaniMadh <mattn.alfred@gmail.com>
2022-01-03 12:15:23 +0100

    speed Improvements

--- Filemanager.java
+++ Filemanager.java
@@ -1,13 +1,26 @@
+package filemanager;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Filemanager {
+
+	static double progress = 1;
+
+	public static double getProgress(){
+		return progress;
+	}
+
 	/**
 	 * Row 1 is the first line
 	 */
@@ -35,7 +48,6 @@ public class Filemanager {
 				fc.set(i, fc.get(i).toString());
 			}
 		}
-
 		String string = String.join("\r\n", fc);
 
 		Filemanager.saveFile(Name, string);
@@ -96,8 +108,11 @@ public class Filemanager {
 	public static String getFileContent(File file) {
 		InputStreamReader fis = null;
 		String Fcontent = "";
-
+		progress = 0;
 		try {
+			Path path = Paths.get(file.getAbsolutePath());
+			long size = Files.size(path);	
+			long status = 0;
 
 			fis = new InputStreamReader(new FileInputStream(file), Charset.forName("UTF-8"));
 
@@ -108,6 +123,8 @@ public class Filemanager {
 				// convert to char and display it
 				// System.out.print((char) content);
 				Fcontent += (char) content;
+				status += 2;
+				progress = (double)((double)status/(double)size);
 			}
 
 		} catch (IOException e) {
@@ -124,6 +141,34 @@ public class Filemanager {
 		return Fcontent;
 	}
 
+	public static ArrayList<String> readFileInArrayList(File f){
+		ArrayList<String> list = new ArrayList<String>();
+		BufferedReader reader;
+		progress = 0;
+
+        try {
+			Path path = Paths.get(f.getAbsolutePath());
+			long size = Files.size(path);	
+			long status = 0;
+
+            reader = new BufferedReader(new FileReader(
+                    f.getAbsolutePath()));
+            String line = "-1";
+            while (line != null) {
+                line = reader.readLine();
+				if(line != null)
+					status += line.getBytes().length;				
+				progress = (double)status/(double)size;
+                list.add((line!=null)?line:"");
+            }
+			progress = 1;
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+	}
+
 	public static String[] getFileContentArray(String Name) {
 		// String string = Filemanager.getFileContent(Name);
 		// String lines[] = string.split("\\r?\\n");
@@ -138,9 +183,10 @@ public class Filemanager {
 	}
 
 	public static ArrayList<String> getFileContentArrayList(File f) {
-		String str = getFileContent(f);
-		ArrayList<String> res = new ArrayList<String>(Arrays.asList(str.split(System.lineSeparator())));
-		return res;
+		// String str = getFileContent(f);
+		// ArrayList<String> res = new ArrayList<String>(Arrays.asList(str.split(System.lineSeparator())));
+		// return res;
+		return readFileInArrayList(f);
 	}
 
 	public static ArrayList<String> getFileContentArrayList(String s) {
